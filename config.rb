2715251c475c09f430182420bdbd7abe5e2b76e0
skycity25 <skycity25@gmail.com>
2017-12-18 22:50:54 +0800

    1.安裝 bootstrap v4.0.0-beta.2 2.移除 compass 3.重新設定 mixin/variables

--- config.rb
+++ config.rb
@@ -5,7 +5,7 @@ require 'compass/import-once/activate'
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
-images_dir = "img"
+images_dir = "images"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
@@ -20,6 +20,6 @@ line_comments = false
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
+preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
